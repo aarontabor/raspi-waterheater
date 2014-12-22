fb14cfd73da3aa2da8f35e4525bb33bbef8f1a71
Aaron W. Tabor <aarontabor50@gmail.com>
2014-12-22 15:12:29 -0400

    Interface for communicating with Therm Sensors.
    Mix Temperature is still stubbed.

---
+++ pi/thermSensor.py
@@ -0,0 +1,26 @@
+from w1thermsensor import W1ThermSensor
+
+tankSensorIds = []
+mixSensorIds = []
+
+
+class ThermSensor:
+  def __init__(self):
+    self.tankSensors = []
+    self.mixSensors = []
+
+    for sensor in W1ThermSensor.get_available_sensors():
+      if sensor.id in tankSensorIds:
+        self.tankSensors.append(sensor)
+      elif sensor.id in mixSensorIds:
+        self.mixSensors.append(sensor)
+
+  def getMixTemperature(self):
+    pass
+
+  def getTankTemperature(self):
+    temperatures = [s.get_temperature() for s in self.tankSensors]
+    return average(temperatures)
+
+def average(numbers):
+  return sum(numbers) / len(numbers)

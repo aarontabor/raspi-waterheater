38c39d14c854535074601b26d34a1590b3be0f52
Aaron W. Tabor <aarontabor50@gmail.com>
2015-01-18 16:39:51 -0400

    Database Schema.
    Initialization file for db.

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE raspi_waterheater;
+USE raspi_waterheater;
+
+CREATE TABLE tank_temperatures(
+  id INT NOT NULL AUTO_INCREMENT,
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  temperature DOUBLE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE output_temperatures(
+  id INT NOT NULL AUTO_INCREMENT,
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  temperature DOUBLE NOT NULL,
+  PRIMARY KEY (id)
+);

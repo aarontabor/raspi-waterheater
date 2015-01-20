e5920055b97b5c9d18010ac5fc1f166b4d90cb18
Aaron W. Tabor <aarontabor50@gmail.com>
2015-01-20 17:18:52 +0000

    Fix. Temperature Sensor Codes are 12 digits.

--- pi/settings.py
+++ pi/settings.py
@@ -16,9 +16,9 @@ Identification numbers can be found by connecting a single sensor to the
 raspberry pi and using the provided utility script:
   python queryConnectedThermSensors.py
 
-Identification numbers are encoded in hexadecimal and are generally 11 digits in
+Identification numbers are encoded in hexadecimal and are generally 12 digits in
 length, for example:
-  0001234abcd
+  00001234abcd
 
 Fill in the current sensor configuration (between the square brackets - []).
 Ensure that identification numbers are:
@@ -26,8 +26,8 @@ Ensure that identification numbers are:
   - are comma separated
 
 Example:
-  tankSensorIds = ['0001234abcd', '0005678bcde']
-  outputSensorIds = ['0001a2b3c4d']
+  tankSensorIds = ['00001234abcd', '00005678bcde']
+  outputSensorIds = ['00001a2b3c4d']
 '''
 tankSensorIds = []
 outputSensorIds = []
